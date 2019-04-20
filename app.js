92feee42bd3d4bae1a1a1223c94b56befd38acca
hrishikeshsoman <hrishikeshsoman3@gmail.com>
2019-04-20 10:48:49 +0530

    Added minimal express router

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+});
+
+module.exports = app;
