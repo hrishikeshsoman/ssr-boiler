d0beee6faa472da1081bdb65b9dffe65209ee84e
hrishikeshsoman <hrishikeshsoman3@gmail.com>
2019-04-20 14:05:54 +0530

    added nodemon

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('hello there')
 });
 
 module.exports = app;
